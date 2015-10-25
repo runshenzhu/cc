2aadc07e3bd94a36563d31377b1d66042609dafb
runshenzhu <runshen.zhu@gmail.com>
2015-10-25 13:57:54 -0400

    connect to table ok

--- p1q2/HbaseHandler.java
+++ p1q2/HbaseHandler.java
@@ -7,24 +7,39 @@ import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.filter.CompareFilter;
 import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
+import org.apache.hadoop.hbase.rest.client.Client;
+import org.apache.hadoop.hbase.rest.client.Cluster;
+import org.apache.hadoop.hbase.rest.client.RemoteHTable;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
-import java.util.List;
+import java.nio.ByteBuffer;
+
+//import java.util.List;
 
 /**
  * Created by zrsh on 10/24/15.
  */
 public class HbaseHandler {
-    final private static String EMR = "ec2-54-173-198-62.compute-1.amazonaws.com";
-    final private static String TABLE = "test";
-    final private static String FAMILY = "obgun";
-    private HTable table = null;
-    public HbaseHandler() throws IOException{
+    private String EMR = "ec2-52-91-91-20.compute-1.amazonaws.com";
+    private String TABLE = "test";
+    private String FAMILY = "obgun";
+    private RemoteHTable table = null;
+    public HbaseHandler(String emr, String table, String family) throws IOException{
+        this.EMR = emr;
+        this.TABLE = table;
+        this.FAMILY = family;
+
         Configuration config = HBaseConfiguration.create();
         config.set("hbase.zookeeper.quorum", this.EMR);
         config.set("hbase.zookeeper.property.clientPort","2181");
-        config.set("hbase.master", this.EMR + ":60000");
+        // config.set("hbase.master", this.EMR + ":60000");
+        Cluster cluster = new Cluster();
+        cluster.add(this.EMR, 8080); // co RestExample-1-Cluster Set up a cluster list adding all known REST server hosts.
+
+        Client client = new Client(cluster); // co RestExample-2-Client Create the client handling the HTTP communication.
+
+        this.table = new RemoteHTable(client, this.TABLE); // co RestExample-3-Table Create a remote table instance, wrapping the REST access into a familiar interface.
 
         try {
             HBaseAdmin.checkHBaseAvailable(config);
@@ -35,25 +50,22 @@ public class HbaseHandler {
         }catch (ZooKeeperConnectionException e){
             System.out.println("zookeeper connect failed");
         }
-        HBaseAdmin admin = new HBaseAdmin(config);
-        String[] tables = admin.getTableNames();
-        for(String table : tables){
-            System.out.println(table);
-        }
-        this.table = new HTable(config, this.TABLE);
+
+
+        //this.table = new HTable(config, this.TABLE);
         System.out.println("connect success");
     }
 
-/*
+
     public void test() throws IOException{
         FilterList list = new FilterList(FilterList.Operator.MUST_PASS_ONE);
         //     scan 'songdata', {COLUMNS => ['data:title'], FILTER => "SingleColumnValueFilter('data', 'title', = , 'regexstring:Total.*Water')"}
 
         SingleColumnValueFilter filter1 = new SingleColumnValueFilter(
-                Bytes.toBytes("data"),
-                Bytes.toBytes("artist_name"),
+                Bytes.toBytes(this.FAMILY),
+                Bytes.toBytes("create_at"),
                 CompareFilter.CompareOp.EQUAL,
-                Bytes.toBytes("Usher Featuring Shyne_ Twista & Kanye West")
+                Bytes.toBytes("2014-05-25+05:37:1")
         );
         list.addFilter(filter1);
 
@@ -65,10 +77,10 @@ public class HbaseHandler {
                 Bytes.toBytes("regexstring:^(Apologies|Confessions).*")
         );
         // list.addFilter(filter2);
-        // Scan s = new Scan();
-        // s.addColumn(Bytes.toBytes("data"), Bytes.toBytes("artist_name"));
-        // s.addColumn(Bytes.toBytes("data"), Bytes.toBytes("title"));
-        // s.setFilter(list);
+        Scan s = new Scan();
+        s.addColumn(Bytes.toBytes(this.FAMILY), Bytes.toBytes("user_id"));
+        s.addColumn(Bytes.toBytes(this.FAMILY), Bytes.toBytes("create_at"));
+        s.setFilter(list);
         ResultScanner scanner = table.getScanner(s);
         try {
             // Scanners return Result instances.
@@ -76,7 +88,14 @@ public class HbaseHandler {
             for (Result rr = scanner.next(); rr != null; rr = scanner.next()) {
                 // print out the row we found and the columns we were looking for
                 for(KeyValue kv : rr.list()){
-                    System.out.println(kv.getKey().toString() + " " + kv.getValue().toString());
+                    ByteBuffer bb = ByteBuffer.wrap(kv.getRow());
+                    System.out.println(bb.getLong());
+                    String qul = new String(kv.getQualifier(), "UTF-8");
+                    System.out.println(qul);
+                    System.out.println(new String(kv.getValue(), "UTF-8"));
+
+                    //System.out.println(kv.getValue());
+                    System.out.println("----");
                 }
             }
             System.out.println("gg");
@@ -92,18 +111,19 @@ public class HbaseHandler {
             scanner.close();
         }
     }
-*/
+
     public void insert(TweetStructure t){
 
         Put p = new Put(Bytes.toBytes(Long.toString(t.id)));
         p.add(Bytes.toBytes(this.FAMILY), Bytes.toBytes("user_id"), Bytes.toBytes(Long.toString(t.userId)));
         p.add(Bytes.toBytes(this.FAMILY), Bytes.toBytes("create_at"), Bytes.toBytes(t.timestamp));
-        p.add(Bytes.toBytes(this.FAMILY), Bytes.toBytes("text "), Bytes.toBytes(t.censoredText));
+        p.add(Bytes.toBytes(this.FAMILY), Bytes.toBytes("text"), Bytes.toBytes(t.censoredText));
         p.add(Bytes.toBytes(this.FAMILY), Bytes.toBytes("sentiment"), Bytes.toBytes(Long.toString(t.score)));
         try {
             table.put(p);
         }catch (IOException e){
             System.out.println("put error");
+            e.printStackTrace();
         }
     }
 
