f571a68fc29fc4ab46baba14e3e6c72fbd21ebf3
omegaga <omegaleedh@gmail.com>
2015-10-25 22:20:20 -0400

    fix table schema

--- p1q2/scripts/mysql/import_data.sql
+++ p1q2/scripts/mysql/import_data.sql
@@ -5,4 +5,4 @@ FIELDS TERMINATED BY ','
       ENCLOSED by '"'
 LINES TERMINATED BY '\n'
 (id, user_id, @var1, text, sentiment)
-SET create_at = STR_TO_DATE(@var1, '%b-%d-%Y+%T');
+SET create_at = STR_TO_DATE(@var1, '%Y-%m-%d+%T');
