97cd1e83bba10474464ae6d63d52e280d8755ecd
omegaga <omegaleedh@gmail.com>
2015-10-26 01:44:00 -0400

    use utf8mb4 as character set

--- p1q2/scripts/mysql/init.sql
+++ p1q2/scripts/mysql/init.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS twitter;
-CREATE DATABASE twitter CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE twitter CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE twitter;
 
 DROP TABLE IF EXISTS tweets;
@@ -8,6 +8,6 @@ CREATE TABLE tweets (
   id bigint,
   user_id bigint,
   create_at bigint,
-  text varchar(640) character set utf8,
+  text varchar(640) character set utf8mb4,
   sentiment int
-)  CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE = INNODB;
+)  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci ENGINE = INNODB;
