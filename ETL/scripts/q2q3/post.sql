1a926fc7ee4d9239d34a8a92ca5dddfb150bbd14
omegaga <omegaleedh@gmail.com>
2015-11-09 11:08:48 -0500

    update mysql scripts

--- ETL/scripts/q2q3/post.sql
+++ ETL/scripts/q2q3/post.sql
@@ -1,1 +1,4 @@
 CREATE INDEX tweets_timestamp_index ON tweets (user_id ASC, create_at ASC) USING BTREE;
+CREATE USER 'obgun' IDENTIFIED BY 'D807isfuckingyou';
+GRANT PRIVILEGES ON twitter.* TO 'obgun'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
