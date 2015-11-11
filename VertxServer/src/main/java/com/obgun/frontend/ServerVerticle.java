46595e96589d2f8e8fe10d16344a4592c7d413f2
runshenzhu <runshen.zhu@gmail.com>
2015-11-11 03:54:35 -0500

    add hbase q3

--- VertxServer/src/main/java/com/obgun/frontend/ServerVerticle.java
+++ VertxServer/src/main/java/com/obgun/frontend/ServerVerticle.java
@@ -30,32 +30,8 @@ public class ServerVerticle extends AbstractVerticle {
         routingContext.response().end("xx CC");
     }
 
-  final private void handleQ2MySQL(RoutingContext routingContext) {
-      vertx.<String>executeBlocking(future -> {
 
-          // Do the blocking operation in here
-
-          // Imagine this was a call to a blocking API to get the result
-          String userid = routingContext.request().getParam("userid");
-          String timeStamp = routingContext.request().getParam("tweet_time");
-          String result = "Omegaga's Black Railgun,6537-0651-1730\n";
-          try {
-              result += SQLHandler.getSqlAnswerQ2(userid, timeStamp);
-          } catch (Exception ignore) {
-              System.out.println("Q2 bad request: " + userid + " " + timeStamp);
-          }
-          future.complete(result);
-
-      }, false, res -> {
-          if (res.succeeded()) {
-              routingContext.response().putHeader("content-type", "text/plain").end(res.result());
-          } else {
-              res.cause().printStackTrace();
-          }
-      });
-  }
-
-  final private void handleQ2(RoutingContext routingContext) {
+  final private void handleQ2HBase(RoutingContext routingContext) {
       vertx.<String>executeBlocking(future -> {
 
           // Do the blocking operation in here
@@ -79,6 +55,32 @@ public class ServerVerticle extends AbstractVerticle {
           }
       });
   }
+    final private void handleQ3HBase(RoutingContext routingContext) {
+        vertx.<String>executeBlocking(future -> {
+
+            // Do the blocking operation in here
+
+            // Imagine this was a call to a blocking API to get the result
+            String result = "Omegaga's Black Railgun,6537-0651-1730\n";
+            try {
+                result += HbaseHandler.getHbaseAnswerQ3(routingContext.request().getParam("userid"),
+                        routingContext.request().getParam("start_date"),
+                        routingContext.request().getParam("end_date"),
+                        routingContext.request().getParam("n"));
+            } catch (Exception ignore) {
+                System.out.println("Q3 bad request: " + routingContext.request().getParam("userid") + " " + routingContext.request().getParam("start_date")
+                +" "+ routingContext.request().getParam("end_date"));
+            }
+            future.complete(result);
+
+        }, false, res -> {
+            if (res.succeeded()) {
+                routingContext.response().putHeader("content-type", "text/plain").end(res.result());
+            } else {
+                res.cause().printStackTrace();
+            }
+        });
+    }
 
     final static private void handleQ2ThreadPool(RoutingContext routingContext){
         final class Worker implements Runnable{
@@ -95,7 +97,8 @@ public class ServerVerticle extends AbstractVerticle {
         threadPool.execute(worker);
     }
 
-    final private void handleQ4(RoutingContext routingContext){
+
+    final private void handleQ4HBase(RoutingContext routingContext){
         vertx.<String>executeBlocking(future -> {
 
             // Do the blocking operation in here
@@ -120,27 +123,43 @@ public class ServerVerticle extends AbstractVerticle {
         });
     }
 
+    final private void handleQ4InMM(RoutingContext routingContext){
+        vertx.<String>executeBlocking(future -> {
+            // Do the blocking operation in here
+
+            // Imagine this was a call to a blocking API to get the result
+            final String hashtag = routingContext.request().getParam("hashtag");
+            final String rank = routingContext.request().getParam("n");
+            final String url = ":80/qq?hashtag="+hashtag+"&n="+rank;
+            String result = "Omegaga's Black Railgun,6537-0651-1730\n";
+            try {
+                result += HttpRequest.sendGet(url);
+            } catch (Exception ignore) {
+                System.out.println("Q4 bad request: " + hashtag + " " + rank);
+            }
+            future.complete(result);
+        }, false, res -> {
+            if (res.succeeded()) {
+                routingContext.response().putHeader("content-type", "text/plain").end(res.result());
+            } else {
+                res.cause().printStackTrace();
+            }
+        });
+    }
+
   @Override
   final public void start() throws Exception {
     // JDBCClient client = JDBCClient.createShared(vertx, config);
-      final String team = "ec2-54-175-191-146.compute-1.amazonaws.com";
+      System.out.println(context.config());
+      final String team = "ec2-54-152-34-253.compute-1.amazonaws.com";
     HbaseHandler.setHbase(team);
-    String[] urls = {
-        "ec2-54-173-250-97.compute-1.amazonaws.com",
-        "ec2-52-91-96-146.compute-1.amazonaws.com",
-        "ec2-54-209-163-93.compute-1.amazonaws.com",
-        "ec2-54-209-161-167.compute-1.amazonaws.com",
-        "ec2-54-209-162-203.compute-1.amazonaws.com",
-        "ec2-54-209-163-194.compute-1.amazonaws.com"
-    };
- //   SQLHandler.setMySql(urls, "obgun", "D807isfuckingyou");
     final Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
     router.get("/q1").handler(ServerVerticle::handleQ1);
-      router.get("/q2").handler(this::handleQ2);
-      router.get("/q4").handler(this::handleQ4);
-    //router.get("/q2").handler(this::handleQ2);
-     // router.get("/q2").handler(this::handleQ2MySQL);
+    //router.get("/q2").handler(ServerVerticle::handleQ2ThreadPool);
+      router.get("/q2").handler(this::handleQ2HBase);
+      router.get("/q3").handler(this::handleQ3HBase);
+      router.get("/q4").handler(this::handleQ4HBase);
       router.get("/heartbeat").handler(ServerVerticle::handleHeartBeat);
     vertx.createHttpServer()
         .requestHandler(router::accept)
